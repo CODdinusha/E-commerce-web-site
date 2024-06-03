7edf9495740b19bd0a113848d8c2527f22e8ae59
Dinusha_Madhuranga <dinushamadhuranga77@gmail.com>
2024-06-03 19:41:19 +0530

    1 st day

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,25 @@
 
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
-
+import { BrowserRouter as Router,Routes,Route, BrowserRouter } from 'react-router-dom'
+import ShopCategory from './Pages/ShopCategory';
 function App() {
   return (
     <div>
-        <Navbar/>
+      <BrowserRouter>
+      <Navbar/>
+      <Routes>
+          <Route path='/' element={<Shop/>}/>
+          <Route path='/mens' element={<ShopCategory category="men"/>}/>
+          <Route path='/womens' element={<ShopCategory category="women"/>}/>
+          <Route path='/kids' element={<ShopCategory category="kid"/>}/>
+          <Route path=':productId' element={<Product/>}/>
+
+          </Route>
+          
+      </Routes>
+      </BrowserRouter>
+        
     </div>
   );
 }
