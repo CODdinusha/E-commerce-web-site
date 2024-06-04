fa8d48159bc2645296270c6a05ac985d9c765bf8
Dinusha_Madhuranga <dinushamadhuranga77@gmail.com>
2024-06-05 01:48:24 +0530

    error fixing

--- frontend/src/Components/Popular/Popular.jsx
+++ frontend/src/Components/Popular/Popular.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './Popular.css'
 import data_product  from '../Assets/data'
-import Item from '../Item/Item'
+
 const Popular = () => {
   return (
     <div className='popular'>
@@ -10,7 +10,7 @@ const Popular = () => {
         <div className="popular-item">
             {data_product.map((item,i)=>{
                 
-                return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price}/>
+                return < 
             })}
         </div>
     </div>
