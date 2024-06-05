93efcf062292f63117bca0db78fabf275b33cabb
Dinusha_Madhuranga <dinushamadhuranga77@gmail.com>
2024-06-06 03:30:38 +0530

    3rd day

--- frontend/src/Components/Assets/data.js
+++ frontend/src/Components/Assets/data.js
@@ -1,35 +1,37 @@
-import p1_img from './product_1.png'
-import p2_img from './product_2.png'
-import p3_img from './product_3.png'
-import p4_img from './product_4.png'
+import p1_img from './product_1.webp'
+import p2_img from './product_2.webp'
+import p3_img from './product_3.webp'
+import p4_img from './product_4.webp'
 
 let data_product = [
     {
         id:1,
-        name:"Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+        name:"Women V-Neck Fashion Tunic Vest Tank Tops Tees T-Shirt Sexy Lace Pullover Tee ",
         image:p1_img,
-        new_price:50.00,
-        old_price:80.50,
+        new_price:4.09,
+        old_price:6.99,
     },
     {
         id:2,
-        name:"Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+        name:"Fashion Women Summer Printed V Neck Camisole Sleeveless Tee Vest Tank Top Blouse",
         image:p2_img,
-        new_price:50.00,
-        old_price:80.50,
+        new_price:3.49,
+        old_price:5.85,
     },
     {
         id:3,
-        name:"Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+        name:"World Map Printed Women's Long Sleeve T Shirt Blouse Tops T-Shirt Fashion Tunic",
         image:p3_img,
         new_price:50.00,
         old_price:80.50,
     },
     {
         id:4,
-        name:"Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+        name:"Fashion Ladies Summer Short Sleeve Tunic Women V Neck Ribbed T-Shirt Plus Size",
         image:p4_img,
         new_price:50.00,
         old_price:80.50,
     },
 ]
+
+export default data_product ;
--- frontend/src/Components/Popular/Popular.jsx
+++ frontend/src/Components/Popular/Popular.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import './Popular.css'
 import data_product  from '../Assets/data'
+import Item from '../item/item'
+
 
 const Popular = () => {
   return (
@@ -10,8 +12,9 @@ const Popular = () => {
         <div className="popular-item">
             {data_product.map((item,i)=>{
                 
-                return < 
-            })}
+                return < Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price} />
+              })}
+            
         </div>
     </div>
   )
