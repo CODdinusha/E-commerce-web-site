aa0f9e99ba59850b78ef092b48d2bb50a32e16ef
Dinusha_Madhuranga <dinushamadhuranga77@gmail.com>
2024-06-07 02:35:36 +0530

    1 st page is done

---
+++ frontend/src/Components/Assets/new_collections.js
@@ -0,0 +1,69 @@
+import n1_img from './new_1.webp'
+import n2_img from './new_2.webp'
+import n3_img from './new_3.webp'
+import n4_img from './new_4.webp'
+import n5_img from './new_5.webp'
+import n6_img from './new_6.webp'
+import n7_img from './new_7.webp'
+import n8_img from './new_8.webp'
+
+let new_collections = [
+    {
+        id:1,
+        name:"Men's T-shirt Long sleeved Cotton Hemp Top Casual Fashion Handsome ",
+        image:n1_img,
+        new_price:10.09,
+        old_price:15.99,
+    },
+    {
+        id:2,
+        name:"New Men Diamond Fashion Rhinestones Lion Short Sleeve Luxury Black White T-Shirt",
+        image:n2_img,
+        new_price:51.90,
+        old_price:70.55,
+    },
+    {
+        id:3,
+        name:"Kids Girls Crop Top NYC Brooklyn Printed Hooded Top & Fashion Legging Outfit Set",
+        image:n3_img,
+        new_price:12.99,
+        old_price:16.50,
+    },
+    {
+        id:4,
+        name:"Kids Girls Plain Khaki Color Jackets Tunic Fleece Collared Fashion Coat 7-13 Yrs",
+        image:n4_img,
+        new_price:5.99,
+        old_price:8.50,
+    },
+    {
+        id:5,
+        name:"Men's Spring and summer Thin Breathable Loose Fashion Casual Jacket Coat ",
+        image:n5_img,
+        new_price:37.58,
+        old_price:53.69,
+    },
+    {
+        id:6,
+        name:"Men's Double Breasted Trench Coat Fashion Solid Casual Long Jacket with Belt",
+        image:n6_img,
+        new_price:41.92,
+        old_price:55.89,
+    },
+    {
+        id:7,
+        name:"Fashion Men Teens Predator Print T-Shirts Summer Casual Short Sleeve TShirt",
+        image:n7_img,
+        new_price:14.76,
+        old_price:16.40,
+    },
+    {
+        id:8,
+        name:"Fashion Mens Shirts Tops Long Sleeve Casual Button Down Shirt Classic Blouse Tee",
+        image:n8_img,
+        new_price:17.18,
+        old_price:25.66,
+    },
+]
+
+export default new_collections ;
